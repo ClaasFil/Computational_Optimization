41bb34ded37960ce64e633c2d6c7de4659cac2ca
maaikeelgersma <maaike.elgersma@gmail.com>
2024-09-25 13:53:37 +0200

    Add simple algorithm (not working yet)

---
+++ simple_greedy/simple_algorithm.py
@@ -0,0 +1,25 @@
+from src.instance import Instance
+
+def execute_simple_algorithm(instance: Instance):
+
+    # Sort deliveries from first available time to last
+
+    # Assign deliveries to drivers
+    # assignments = dict { driver: list[deliveries]}
+
+    # Compute total delivery time of each driver
+    # delivery_times = dict { driver: int }
+    # for driver in drivers:
+    # total_t = 0
+    # for delivery in assignments[driver]:
+    # total_t += max(total_t + delivery_time[startlocation,pickuplocation[delivery]], start_time[delivery])
+    # total_t += deliverytime[pickuplocation,dropofflocation[delivery]]
+    # delivery_times[driver] = total_t
+
+    # Compute objective of problem
+    # objective =  #int
+
+    # Translate solution to variables
+    # solution = 
+
+    return # solution
