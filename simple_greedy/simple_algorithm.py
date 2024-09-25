0c49f912515deef666dd65e702f0e6312027dee4
maaikeelgersma <maaike.elgersma@gmail.com>
2024-09-25 16:12:55 +0200

    Update execute_simple_algorithm to update courier.activities

--- simple_greedy/simple_algorithm.py
+++ simple_greedy/simple_algorithm.py
@@ -6,18 +6,40 @@ import logging
 def execute_simple_algorithm(instance: Instance):
 
     # Idea of the algorithm:
-    # Sort deliveries from first available time to last
+    # Sort deliveries from first available time to last.
     # Assign the first of these sorted deliveries to first courier, second to second, etc., 
-    # and keep louping over couriers until all deliveries are assigned
-    # We do not stack, so keeping track of capacities is not necessary
-    # 180 min per route might be violated, it will indicate that
-    # It will say that the solution is infeasible if there are more than 4 deliveries per courier
-
+    # and keep louping over couriers until all deliveries are assigned.
+    # We do not stack, so keeping track of capacities is not necessary.
+    # We do not guarantee a feasible solution,
+    # 180min per route and max4 deliveries per courier might be violated.
+    
     n = len(instance.couriers)
     m = len(instance.deliveries)
 
     # Sort deliveries from first available time to last
     sorted_deliveries = sorted(instance.deliveries, key=lambda delivery: delivery.time_window_start)
+
+    # Assign deliveries to drivers
+    courier_id = 1
+    for delivery in sorted_deliveries:
+        # identify next delivery for our courier
+        courier = list(filter(lambda elem: elem.courier_id == courier_id, instance.couriers))[0]
+        # add pickup to acitivity list of courier
+        courier.activities.append(delivery.delivery_id)
+        # add drop off to activity list of courier
+        courier.activities.append(delivery.delivery_id)
+        courier_id += 1
+        if courier_id > n:
+            courier_id = 1
+    for courier in instance.couriers:
+        print(courier.activities)
+
+    # DO NOT NEED TO RUN THE CODE BELOW FOR FEASIBILITY CHECKER 
+    # just for checking 180min constraint and max4 deliveries constraint
+    # and finding interpretable solution and objective function value
+
+    # 180 min per route might be violated, it will indicate that
+    # It will say that the solution is infeasible if there are more than 4 deliveries per courier
 
     # Assign deliveries to drivers
     assignments = {courier_id: [] for courier_id in range(1,n+1)} # dict {courier: list[deliveries]}
@@ -27,15 +49,15 @@ def execute_simple_algorithm(instance: Instance):
         courier_id += 1
         if courier_id > n:
             courier_id = 1
-    
+
     # Check max. 4 deliveries per courier constraint
     for courier_id, delivery_lst in assignments.items():
         if len(delivery_lst) > 4:
             logging.error("Oops! Courier" + str(courier.courier_id) + "has to deliver more than 4 deliveries")
             raise Exception("So this is not a feasible solution.")
 
-    # Compute total delivery time of each driver
-    # delivery_times = {courier_id: 0 for courier_id in range(n+1,n+m+1)} # dict {courier: int}
+    # Find the solution provided by this assignment:
+    # The total delivery time (objective funciton value) and the list of activities for each courier
     total_delivery_time = 0 # objective function value
     for courier in instance.couriers:
         total_t = 0
@@ -45,8 +67,10 @@ def execute_simple_algorithm(instance: Instance):
             delivery = list(filter(lambda elem: elem.delivery_id == delivery_id, instance.deliveries))[0]
             # calculate travel time from couriers current location to pickup location
             total_t += max(total_t + instance.travel_time[courier_loc][delivery.pickup_loc], delivery.time_window_start)
+            courier.activities.append(delivery.delivery_id)
             # caclulate travel time from pickup to delivery locatiion
             total_t += instance.travel_time[delivery.pickup_loc][delivery.dropoff_loc]
+            courier.activities.append(delivery.delivery_id)
             # add this dropoff time to the total delivery time
             total_delivery_time += total_t
             # set current courier location to dropoff location
@@ -58,7 +82,4 @@ def execute_simple_algorithm(instance: Instance):
         # # get delivery driver home
         # total_t += instance.travel_time[delivery.dropoff_loc][courier.location]
 
-    # Translate solution to variables
-    # solution = 
-
-    return # solution
+    return 0
